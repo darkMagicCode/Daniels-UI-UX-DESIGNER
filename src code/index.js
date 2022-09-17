66691320b8e9f0269ec3866dba7cff18f76c1310
darkMagic Code <111946821+darkMagicCode@users.noreply.github.com>
2022-09-17 05:33:54 +0200

    Add files via upload

---
+++ src code/index.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.text-typing', {
+    strings: [' larry danials ^500', 'devevloper ^450', 'designer ^450'],
+    typeSpeed: 40,
+    backSpeed: 30,
+    loop: true,
+    smartBackspace: true 
+  
+  
+  });
+  
+  
